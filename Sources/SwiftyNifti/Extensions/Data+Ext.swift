c850429d0a84904e39fefb391cf6e95c1f1895f1
Charles Pinesky <25388414+Vaidios@users.noreply.github.com>
2025-06-30 20:55:12 +0200

    Fix volume loading and data reads

--- Sources/SwiftyNifti/Extensions/Data+Ext.swift
+++ Sources/SwiftyNifti/Extensions/Data+Ext.swift
@@ -3,17 +3,21 @@ import Foundation
 extension Data {
   
   var byteSwapped: Data { Data(reversed()) }
-  
+
   func loadVector<T>(at offset: Int = 0, length: Int, isByteSwapped: Bool) -> [T] {
     (0 ..< length).map { load(at: offset + $0 * MemoryLayout<T>.size, isByteSwapped: isByteSwapped) }
   }
-  
+
   func load<T>(at offset: Int, isByteSwapped: Bool = false) -> T {
-    let subdata = subdata(in: offset ..< offset + MemoryLayout<T>.size)
-    return isByteSwapped ? subdata.byteSwapped.load() : subdata.load()
+    let end = offset + MemoryLayout<T>.size
+    precondition(end <= count, "Read out of bounds offset \(offset) size \(MemoryLayout<T>.size) count \(count)")
+    var buffer = [UInt8](repeating: 0, count: MemoryLayout<T>.size)
+    copyBytes(to: &buffer, from: offset ..< end)
+    if isByteSwapped { buffer.reverse() }
+    return buffer.withUnsafeBytes { $0.loadUnaligned(as: T.self) }
   }
-  
+
   func load<T>() -> T {
-    withUnsafeBytes { $0.load(as: T.self) }
+    withUnsafeBytes { $0.loadUnaligned(as: T.self) }
   }
 }
